1367ef8b84ee2cdde7f9ed06b1669396ceb5413c
Alexsandr Valentinov <djvalentinoff@gmail.com>
2024-03-07 17:42:59 +0300

    init: starn project and initial training project

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import "./_vars.scss";
+import "./_reset.scss";
+import "./_base.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
