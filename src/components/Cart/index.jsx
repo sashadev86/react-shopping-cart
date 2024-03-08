c9dbbed616c5a14571fb42d3eeb7e5777492495c
Alexsandr Valentinov <djvalentinoff@gmail.com>
2024-03-08 11:21:22 +0300

    feat: product cart and delete button

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import CartFooter from '../CartFooter';
 import CartHeader from '../CartHeader';
 import Product from '../Product';
+import data from "../../data";
 
 const Cart = () => {
+  const [cart, setCart] = useState(data);
+
+  const deleteProduct = (id) => {
+    setCart(cart.filter((product) => product.id !== id));
+  };
+
+  const products = cart.map((product) => {
+    return <Product product={product} deleteProduct={deleteProduct} key={product.id} />;
+  });
+
   return ( 
     <section className="cart">
       <CartHeader />
-      <Product />
+      {products}
       <CartFooter />
     </section>
    );
